fc27dd1afba648fa729c1674ff4799c6b83e0f45
PANdas <krsy0411@naver.com>
2024-11-25 22:07:33 +0900

    [jwt] 인증 미들웨어 부착

--- Authentication/jwt/index.js
+++ Authentication/jwt/index.js
@@ -4,11 +4,43 @@ const jwt = require("jsonwebtoken");
 const PORT = 4000;
 const SECRET_TOKEN = "secretToken";
 
+const posts = [
+	{
+		username: "John",
+		title: "Post 1",
+	},
+	{
+		username: "Jim",
+		title: "Post 2",
+	},
+];
+
 const app = express(); // express app 생성
 
-// middleware
+function authMiddleWare(req, res, next) {
+	const authHeader = req.headers["authorization"];
+	const token = authHeader && authHeader.split(" ")[1];
+
+	if (token == null) {
+		return res.sendStatus(401);
+	}
+
+	jwt.verify(token, SECRET_TOKEN, (err, user) => {
+		if (err) {
+			return res.sendStatus(403);
+		}
+
+		// 다음 미들웨어에서 user 정보를 사용할 수 있도록 req에 추가
+		req.user = user;
+		// 다음 미들웨어로 이동
+		next();
+	});
+}
+
+// middleware(global)
 app.use(express.json()); // application/json 파싱
 
+// http method
 app.post("/login", (req, res) => {
 	const username = req.body.username;
 	const user = { name: username };
@@ -16,6 +48,10 @@ app.post("/login", (req, res) => {
 	const accessToken = jwt.sign(user, SECRET_TOKEN);
 
 	res.json({ accessToken });
+});
+
+app.get("/posts", authMiddleWare, (req, res) => {
+	res.json(posts);
 });
 
 // 실행
