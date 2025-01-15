3c822f0ebc8c6f7963d1b8c57e20e11bb76f8c91
PANdas <krsy0411@naver.com>
2025-01-15 19:26:24 +0900

    ejs 모듈 : 간단한 UI 적용

--- Authentication/passport/src/index.js
+++ Authentication/passport/src/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
-const path = require("path");
+const path = require("path"); // 경로 설정을 위한 모듈
 const app = express();
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const PORT = 4000;
-const CONNECT_STRING =
-	"mongodb+srv://lee:469FkxGZ98BPy9aU@lee.9vryi.mongodb.net/?retryWrites=true&w=majority&appName=LEE";
+const CONNECT_STRING = process.env.CONNECT_STRING;
 
 app.use(express.json());
 /* URL-encoded 형식의 요청 본문을 구문 분석(parsing)하는 미들웨어를 설정 :
 HTML 폼에서 전송된 데이터를 해석하여 req.body 객체에 저장 */
 app.use(express.urlencoded({ extended: false }));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 mongoose
 	.connect(CONNECT_STRING)
@@ -22,7 +26,13 @@ mongoose
 	});
 
 app.use("/static", express.static(path.join(__dirname, "public")));
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+app.get("/signup", (req, res) => {
+	res.render("signup");
+});
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port localhost:${PORT}`);
 });
--- Authentication/passport/src/models/users.js
+++ Authentication/passport/src/models/users.js
@@ -15,6 +15,7 @@ const userSchema = new mongoose.Schema({
 	googleId: {
 		type: String,
 		unique: true,
+		sparse: true,
 	},
 });
 
